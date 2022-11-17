79f958f425abcb27d20af200c61a7b3364b2a22e
pratikshabansod07 <pratikshabansod07@gmail.com>
2022-11-17 15:49:10 +0530

    my second commit

---
+++ Project1/src/com/velocity/git/practice/Demo2.java
@@ -0,0 +1,22 @@
+package com.velocity.git.practice;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		String str="Pratikshashk";
+		char[] ch=str.toCharArray();
+		for(int i=0;i<str.length();i++)
+		{
+			for(int j=i+1;j<str.length();j++)
+			{
+				if(ch[i]==ch[j])
+				{
+					System.out.println(ch[i]);
+					break;
+				}
+			}
+			
+		}
+	}
+
+}
